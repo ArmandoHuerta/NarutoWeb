412e340905e811a5088f151fb43834db0b0ec579
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2022-11-22 02:08:17 -0600

    Añadir logica para la descripcion del clan.

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,22 @@ app.post("/api", function (req, res) {
   const { clanName } = req.body;
 
   const clanes = {
-    Uchiha: "El Clan Uchiha (´うちは一族´, Uchiha Ichizoku) también conocido como Clan Uchiha del Sharingan (Sharingan no Uchiha Ichizoku) fue considerado uno de los dos clanes más poderosos del mundo shinobi, junto con el Clan Senju, durante la Era de Guerra entre Clanes.",
-    Uzumaki: "El Clan Uzumaki (´うずまき一族´, Uzumaki Ichizoku) fue un prominente clan de Uzushiogakure. Eran parientes lejanos del Clan Senju y por lo tanto ambos clanes estaban en buenos términos, una alianza que se extendía a sus aldeas ocultas - Konohagakure y Uzushiogakure.Algo muy peculiar en ellos es que la mayoria del clan poseen gran cantidad de shakra y en su mayoria son pelirojos.",
-    Hyuga:"El clan Hyūga (´日向一族´ ´Hyūga´ Ichizoku) es un clan del manga y anime Naruto, residente en la Aldea Oculta de Konoha. Hyūga (日向) significa hacia el Sol. Posee una técnica de línea sucesoria denominada Byakugan, que les otorga una visión completa a su alrededor, y la capacidad para ver los movimiento del chakra.",  
+    Uchiha:
+      "El Clan Uchiha (´うちは一族´, Uchiha Ichizoku) también conocido como Clan Uchiha del Sharingan (Sharingan no Uchiha Ichizoku) fue considerado uno de los dos clanes más poderosos del mundo shinobi, junto con el Clan Senju, durante la Era de Guerra entre Clanes.",
+    Uzumaki:
+      "El Clan Uzumaki (´うずまき一族´, Uzumaki Ichizoku) fue un prominente clan de Uzushiogakure. Eran parientes lejanos del Clan Senju y por lo tanto ambos clanes estaban en buenos términos, una alianza que se extendía a sus aldeas ocultas - Konohagakure y Uzushiogakure.Algo muy peculiar en ellos es que la mayoria del clan poseen gran cantidad de shakra y en su mayoria son pelirojos.",
+    Hyuga:
+      "El clan Hyūga (´日向一族´ Hyūga Ichizoku) es un clan del manga y anime Naruto, residente en la Aldea Oculta de Konoha. Hyūga (日向) significa hacia el Sol. Posee una técnica de línea sucesoria denominada Byakugan, que les otorga una visión completa a su alrededor, y la capacidad para ver los movimiento del chakra.",
   };
+  let clanDescripcion = "";
+  if (clanName == "Uchiha") clanDescripcion = clanes.Uchiha;
+  if (clanName == "Uzumaki") clanDescripcion = clanes.Uzumaki;
+  if (clanName == "Hyuga") clanDescripcion = clanes.Hyuga;
 
-  res.cookie("clanSeleccionado", clanName).redirect("/frontend/resultado");
+  res
+    .cookie("clanSeleccionado", clanDescripcion)
+    .cookie("clanDescripcion")
+    .redirect("/frontend/resultado");
 });
 
 app.listen(6369);
